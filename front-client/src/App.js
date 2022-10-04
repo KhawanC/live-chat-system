b85d753a3b9aca34435687915d6cdb80ef3f1821
KhawanC <kauacassiano121@gmail.com>
2022-10-04 13:12:07 +0000

    adding front client to rep

---
+++ front-client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './global-style.css'
+import { Chatroom } from './Pages/Chatroom/Chatroom';
+
+function App() {
+  return (
+    <Chatroom />
+  );
+}
+
+export default App;
